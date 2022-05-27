6a04bfb33c12f69fb1f9297756749968e98461fd
Sergey Bibikov <serjantb1@gmail.com>
2022-05-27 21:19:39 +0300

    Добавил переменную result , сделал цикл, который после каждого прогона записывает новое значение в result

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let num = 266219;
 let massiveNum = Array.from(String(num)).map(i => Number(i));
-console.log(massiveNum)
+//console.log(massiveNum) 
+let result = 1;
+for (let i = 0; i < massiveNum.length; ++i) {
+    result *= massiveNum[i];
+}
+//console.log(result);
